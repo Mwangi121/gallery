7e627e20143e5759f78bee983e2fed48313ab20a
Mwangi121 <collins.mwangi4@student.moringaschool.com>
2025-06-26 12:19:13 +0300

    Updated config .js file to match credentials of MongoDB

--- _config.js
+++ _config.js
@@ -1,9 +1,10 @@
 var config = {}
 
-// Update to have your correct username and password
+//MongoDB Atlas connection strings
 config.mongoURI = {
-    production: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
-    development: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
-    test: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
+    production: 'mongodb+srv://collinsmwangi4:45127Rex@gallery.xhxqb3v.mongodb.net/darkroom?retryWrites=true&w=majority',
+    development: 'mongodb+srv://collinsmwangi4:45127Rex@gallery.xhxqb3v.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
+    test: 'mongodb+srv://collinsmwangi4:45127Rex@gallery.xhxqb3v.mongodb.net/darkroom-test?retryWrites=true&w=majority',
 }
+
 module.exports = config;
