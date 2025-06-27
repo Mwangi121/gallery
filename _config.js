1002a2900fba92c183412439e125a8e7d26c711f
Mwangi121 <collins.mwangi4@student.moringaschool.com>
2025-06-27 15:43:23 +0300

    ediited mongoDB string

--- _config.js
+++ _config.js
@@ -2,9 +2,9 @@ var config = {}
 
 //MongoDB Atlas connection strings
 config.mongoURI = {
-    production: 'mongodb+srv://collinsmwangi4:45127Rex@gallery.xhxqb3v.mongodb.net/darkroom?retryWrites=true&w=majority',
-    development: 'mongodb+srv://collinsmwangi4:45127Rex@gallery.xhxqb3v.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
-    test: 'mongodb+srv://collinsmwangi4:45127Rex@gallery.xhxqb3v.mongodb.net/darkroom-test?retryWrites=true&w=majority',
+    production: 'mongodb+srv://collinsmwangi4:45127Rex@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
+    development: 'mongodb+srv://collinsmwangi4:45127Rex@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
+    test: 'mongodb+srv://collinsmwangi4:45127Rex@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
 }
 
 module.exports = config;
